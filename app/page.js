71a9331dd78c313505907b8014bfa6a9bf28bd7a
Kushagra Pandey <kushagra.15554@gmail.com>
2024-10-21 18:06:53 +0530

    Basic routing and header added

---
+++ app/page.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Time to get started!
+      </h1>
+      <p>
+        <Link href="/meals">Meals Page</Link>
+      </p>
+      <p>
+        <Link href="/community">Community Page</Link>
+      </p>
+    </main>
+  );
+}
